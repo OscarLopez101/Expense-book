e024bc59e54ab969ba8c36874eaff4398df2667f
Student <oscar.lopez102004.ol@gmail.com>
2025-10-14 14:11:07 -0700

    Started Ledger.java file

---
+++ src/main/java/com/pluralsight/Ledger.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Ledger {
+
+    public static void displayLedger(List<Transaction> transactions) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("\nLedger menu");
+            System.out.println("A) All");
+            System.out.println("B) Deposits");
+            System.out.println("P) Payments");
+            System.out.println("R) Reports");
+            System.out.println("H) Home");
+            System.out.println("Chose an option");
+
+        }
+
+
+
+    }
+}
